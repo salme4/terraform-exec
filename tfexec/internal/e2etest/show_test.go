ab42232e6bd76de0853ea2b851ec52c6039c55b9
Paul Tyng <paul@paultyng.net>
2020-09-03 16:44:39 -0400

    Add mac builds, speed up win builds
    Closes #65

--- tfexec/internal/e2etest/show_test.go
+++ tfexec/internal/e2etest/show_test.go
@@ -192,8 +192,8 @@ func TestShowPlanFile012(t *testing.T) {
 	runTestVersions(t, []string{testutil.Latest012}, "non_default_planfile_012", func(t *testing.T, tfv *version.Version, tf *tfexec.Terraform) {
 		// plan file fixture was created in Linux, and is
 		// not compatible with Windows
-		if runtime.GOOS == "windows" {
-			t.Skip("plan file created in 0.12 on Linux is not compatible with Windows")
+		if runtime.GOOS != "linux" {
+			t.Skip("plan file created in 0.12 on Linux is not compatible with other OS")
 		}
 
 		providerName := "null"
